0142f9a61f596fc936bce5cff363a6509ab915ed
Jonas Depoix <jdepoix@seibert-media.net>
2018-04-20 14:20:24 +0200

    "cli" improved

--- youtube_transcript_api.py
+++ youtube_transcript_api.py
@@ -4,7 +4,13 @@ import sys
 
 import json
 
+from pprint import pprint
+
 from src.transcript_api import YouTubeTranscriptApi
 
+
 if __name__ == '__main__':
-    print(json.dumps(YouTubeTranscriptApi.get(*sys.argv[1:])))
+    if sys.argv[1] == '--json':
+        print(json.dumps(YouTubeTranscriptApi.get_transcripts(*sys.argv[2:], continue_after_error=True)[0]))
+    else:
+        pprint(YouTubeTranscriptApi.get_transcripts(*sys.argv[1:], continue_after_error=True)[0])
