94e4e4063f6ea8ff93d0623aecccc1eb2f9acba0
Jonas Depoix <jdepoix@seibert-media.net>
2019-03-14 17:04:34 +0100

    test suite and corresponding tooling added

---
+++ youtube_transcript_api/_cli.py
@@ -0,0 +1,57 @@
+import json
+
+import pprint
+
+import argparse
+
+from ._api import YouTubeTranscriptApi
+
+
+class YouTubeTranscriptCli():
+    def __init__(self, args):
+        self._args = args
+
+    def run(self):
+        parsed_args = self._parse_args()
+
+        transcripts, _ = YouTubeTranscriptApi.get_transcripts(
+            parsed_args.video_ids,
+            languages=parsed_args.languages,
+            continue_after_error=True
+        )
+
+        if parsed_args.json:
+            return json.dumps(transcripts)
+        else:
+            return pprint.pformat(transcripts)
+
+    def _parse_args(self):
+        parser = argparse.ArgumentParser(
+            description=(
+                'This is an python API which allows you to get the transcripts/subtitles for a given YouTube video. '
+                'It also works for automatically generated subtitles and it does not require a headless browser, like '
+                'other selenium based solutions do!'
+            )
+        )
+        parser.add_argument('video_ids', nargs='+', type=str, help='List of YouTube video IDs.')
+        parser.add_argument(
+            '--languages',
+            nargs='*',
+            default=[],
+            type=str,
+            help=(
+                'A list of language codes in a descending priority. For example, if this is set to "de en" it will '
+                'first try to fetch the german transcript (de) and then fetch the english transcipt (en) if it fails '
+                'to do so. As I can\'t provide a complete list of all working language codes with full certainty, you '
+                'may have to play around with the language codes a bit, to find the one which is working for you!'
+            ),
+        )
+        parser.add_argument(
+            '--json',
+            action='store_const',
+            const=True,
+            default=False,
+            help='If this flag is set the output will be JSON formatted.',
+        )
+
+        return parser.parse_args(self._args)
---
+++ youtube_transcript_api/test/test_api.py
@@ -0,0 +1,103 @@
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+import os
+
+import httpretty
+
+from youtube_transcript_api._api import YouTubeTranscriptApi
+
+
+def load_asset(filename):
+    with open('{dirname}/assets/{filename}'.format(dirname=os.path.dirname(__file__), filename=filename)) as file:
+        return file.read()
+
+
+class TestYouTubeTranscriptApi(TestCase):
+    def setUp(self):
+        httpretty.enable()
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.youtube.com/watch',
+            body=load_asset('youtube.html')
+        )
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.youtube.com/api/timedtext',
+            body=load_asset('transcript.xml')
+        )
+
+    def tearDown(self):
+        httpretty.disable()
+
+    def test_get_transcript(self):
+        transcript = YouTubeTranscriptApi.get_transcript('GJLlxj_dtq8')
+
+        self.assertEqual(
+            transcript,
+            [
+                {'text': 'Hey, this is just a test', 'start': 0.0, 'duration': 1.54},
+                {'text': 'this is not the original transcript', 'start': 1.54, 'duration': 4.16},
+                {'text': 'just something shorter, I made up for testing', 'start': 5.7, 'duration': 3.239}
+            ]
+        )
+
+    def test_get_transcript__correct_language_is_used(self):
+        YouTubeTranscriptApi.get_transcript('GJLlxj_dtq8', ['de', 'en'])
+        query_string = httpretty.last_request().querystring
+
+        self.assertIn('lang', query_string)
+        self.assertEqual(len(query_string['lang']), 1)
+        self.assertEqual(query_string['lang'][0], 'de')
+
+    def test_get_transcript__fallback_language_is_used(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.youtube.com/api/timedtext',
+            body=''
+        )
+
+        YouTubeTranscriptApi.get_transcript('GJLlxj_dtq8', ['de', 'en'])
+        query_string = httpretty.last_request().querystring
+
+        self.assertIn('lang', query_string)
+        self.assertEqual(len(query_string['lang']), 1)
+        self.assertEqual(query_string['lang'][0], 'en')
+
+    def test_get_transcript__exception_is_raised_when_not_available(self):
+        httpretty.register_uri(
+            httpretty.GET,
+            'https://www.youtube.com/api/timedtext',
+            body=''
+        )
+
+        with self.assertRaises(YouTubeTranscriptApi.CouldNotRetrieveTranscript):
+            YouTubeTranscriptApi.get_transcript('GJLlxj_dtq8')
+
+    def test_get_transcripts(self):
+        video_id_1 = 'video_id_1'
+        video_id_2 = 'video_id_2'
+        languages = ['de', 'en']
+        YouTubeTranscriptApi.get_transcript = MagicMock()
+
+        YouTubeTranscriptApi.get_transcripts([video_id_1, video_id_2], languages=languages)
+
+        YouTubeTranscriptApi.get_transcript.assert_any_call(video_id_1, languages)
+        YouTubeTranscriptApi.get_transcript.assert_any_call(video_id_2, languages)
+        self.assertEqual(YouTubeTranscriptApi.get_transcript.call_count, 2)
+
+    def test_get_transcripts__stop_on_error(self):
+        YouTubeTranscriptApi.get_transcript = MagicMock(side_effect=Exception('Error'))
+
+        with self.assertRaises(Exception):
+            YouTubeTranscriptApi.get_transcripts(['video_id_1', 'video_id_2'])
+
+    def test_get_transcripts__continue_on_error(self):
+        video_id_1 = 'video_id_1'
+        video_id_2 = 'video_id_2'
+        YouTubeTranscriptApi.get_transcript = MagicMock(side_effect=Exception('Error'))
+
+        YouTubeTranscriptApi.get_transcripts(['video_id_1', 'video_id_2'], continue_after_error=True)
+
+        YouTubeTranscriptApi.get_transcript.assert_any_call(video_id_1, None)
+        YouTubeTranscriptApi.get_transcript.assert_any_call(video_id_2, None)
