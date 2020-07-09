74d36a821e3d7e9cc907bf0ce0fff38e4a627f44
Chris Howell <crhowell@users.noreply.github.com>
2020-07-09 00:06:36 -0700

    Update CLI to make use of new DELIMITER and combine method

--- youtube_transcript_api/_cli.py
+++ youtube_transcript_api/_cli.py
@@ -25,21 +25,24 @@ class YouTubeTranscriptCli():
         transcripts = []
         exceptions = []
 
+        Formatter = formats.get_formatter(parsed_args.format)
+
         for video_id in parsed_args.video_ids:
             try:
-                transcripts.append(self._fetch_transcript(parsed_args, proxies, cookies, video_id))
+                transcript = self._fetch_transcript(
+                    parsed_args, proxies, cookies, video_id)
+                transcripts.append(Formatter.format(transcript))
             except Exception as exception:
                 exceptions.append(exception)
 
-        Formatter = formats.get_formatter(parsed_args.format)
-        results = Formatter.format(transcripts)
-
         return ''.join(
-            [str(exception) for exception in exceptions] + results
+            [str(exception) for exception in exceptions]
+            + ([Formatter.combine(transcripts)] if transcripts else [])
         )
 
     def _fetch_transcript(self, parsed_args, proxies, cookies, video_id):
-        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id, proxies=proxies, cookies=cookies)
+        transcript_list = YouTubeTranscriptApi.list_transcripts(
+                            video_id, proxies=proxies, cookies=cookies)
 
         if parsed_args.list_transcripts:
             return str(transcript_list)
