4418bd8e790d2029cd156731ef2e71195b66bbb4
André Christoffer Andersen <andre@andersen.im>
2024-09-23 22:37:28 +0200

    Adding check for "Sign in to confirm you’re not a bot"

--- youtube_transcript_api/_transcripts.py
+++ youtube_transcript_api/_transcripts.py
@@ -16,6 +16,7 @@ from requests import HTTPError
 from ._html_unescaping import unescape
 from ._errors import (
     VideoUnavailable,
+    BotRestricted,
     TooManyRequests,
     YouTubeRequestFailed,
     NoTranscriptFound,
@@ -58,6 +59,8 @@ class TranscriptListFetcher(object):
                 raise TooManyRequests(video_id)
             if '"playabilityStatus":' not in html:
                 raise VideoUnavailable(video_id)
+            if '"playabilityStatus":' in html and '"Sign in to confirm you’re not a bot"' in html:
+                raise BotRestricted(video_id)
 
             raise TranscriptsDisabled(video_id)
 
